75fcc2bf053b94cbbb7c05fe3baf0388a8641915
ShriyashBPatil <115957962+ShriyashBPatil@users.noreply.github.com>
2026-05-19 16:59:50 +0530

    hero

--- feature.php
+++ feature.php
@@ -20,7 +20,7 @@ include 'header.php';
 
 
     <!-- Fact Start -->
-    <div class="container-xxl py-5">
+    <div class="container-xxl py-5 map-bg">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
@@ -65,9 +65,9 @@ include 'header.php';
 
 
     <!-- Feature Start -->
-    <div class="container-fluid overflow-hidden py-5 px-lg-0">
-        <div class="container feature py-5 px-lg-0">
-            <div class="row g-5 mx-lg-0">
+    <div class="container-xxl py-5">
+        <div class="container py-5">
+            <div class="row g-5 align-items-center">
                 <div class="col-lg-6 feature-text wow fadeInUp" data-wow-delay="0.1s">
                     <h6 class="text-secondary text-uppercase mb-3">Our Features</h6>
                     <h1 class="mb-5">We Are Trusted Logistics Company Since 1990</h1>
@@ -93,10 +93,8 @@ include 'header.php';
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 pe-lg-0 wow fadeInRight" data-wow-delay="0.1s" style="min-height: 400px;">
-                    <div class="position-relative h-100">
-                        <img class="position-absolute img-fluid w-100 h-100" src="img/feature.jpg" style="object-fit: cover;" alt="">
-                    </div>
+                <div class="col-lg-6 wow fadeInRight" data-wow-delay="0.1s">
+                    <img class="img-fluid about-img-custom" src="img/feature.jpg" alt="Our Features">
                 </div>
             </div>
         </div>
